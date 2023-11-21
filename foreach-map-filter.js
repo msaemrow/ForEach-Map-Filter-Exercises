266a6440fe98f124fb3026fe60af3c6e24b904ee
Matt Saemrow <mattsaemrow@gmail.com>
2023-11-21 11:00:01 -0600

    completed filter exercises. have 3 failures on previous tests to fix

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -156,7 +156,11 @@ Examples:
     filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
-function filterByValue(arr, key) {}
+function filterByValue(arr, key) {
+    arr.filter(function(val){
+        return val[key] !== undefined;
+    });
+}
 
 /*
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
@@ -166,7 +170,12 @@ Examples:
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) {}
+function find(arr, searchValue) {
+    let filterVal = arr.filter(function(val){
+        return val === searchValue; 
+    })
+    return filterVal[0];
+}
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
@@ -175,7 +184,12 @@ Examples:
     findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) {}
+function findInObj(arr, key, searchValue) {
+    let filterVals = arr.filter(function(val){
+        return val[key] === searchValue;
+    })
+    return filterVals[0];
+}
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
@@ -186,7 +200,14 @@ Examples:
     removeVowels('ZZZZZZ') // ('zzzzzz')
 */
 
-function removeVowels(str) {}
+function removeVowels(str) {
+    let lowerCase = str.toLowerCase();
+    let strArr = lowerCase.split('');
+    const vowels = 'aeiou';
+    return strArr.filter(function(val){
+        return vowels.indexOf(val) === -1;
+    }).join("");
+}
 
 /*
 Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
@@ -196,4 +217,12 @@ Examples:
     doubleOddNumbers([4,4,4,4,4]) // []
 */
 
-function doubleOddNumbers(arr) {}
+function doubleOddNumbers(arr) {
+    return arr
+    .filter(function(val){
+        return val % 2 !== 0;
+    })
+    .map(function(val){
+        return val * 2;
+    });
+}
